25bdf2f3b0796a0a588a9212538d7756cedab47d
yhjhhhgd <2384947346@qq.com>
2026-06-27 21:42:28 +0800

    Update and rename 1231.txt to user_service.py

---
+++ services/user_service.py
@@ -0,0 +1,10 @@
+from ..models import User
+
+
+def get_user(session, uid, name):
+    user = session.get(User, uid)
+    if not user:
+        user = User(user_id=uid, username=name)
+        session.add(user)
+        session.commit()
+    return user
