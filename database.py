59b06c57e6228d870a96257a0ce92b30466f2773
yhjhhhgd <2384947346@qq.com>
2026-06-27 22:19:04 +0800

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from .config import DATABASE_URL
+from config import DATABASE_URL
 
 Base = declarative_base()
 
