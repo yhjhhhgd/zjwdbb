2b2f10e06f1f91efac384972e6f1defe012685c9
yhjhhhgd <2384947346@qq.com>
2026-07-04 03:35:27 +0800

    Update economy.py

--- core/economy.py
+++ core/economy.py
@@ -3,16 +3,16 @@ import random
 
 def reward(user):
     user.xp += random.randint(8, 15)
-    user.coins += random.randint(5, 12)
-    user.qi += random.randint(1, 3)
+    user.coins += random.randint(1, 5)
+    user.qi += random.randint(8, 15)
 
 
 def level_up(user):
-    while user.xp >= user.level * 120:
-        user.xp -= user.level * 120
+    while user.xp >= user.level * 500:
+        user.xp -= user.level * 500
         user.level += 1
-        user.luck += 0.03
-        user.coins += 25
+        user.luck += 0.05
+        user.coins += 250
 
 
 def inflation_control(user):
