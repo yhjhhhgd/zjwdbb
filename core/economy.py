60f4db3c59fbdc3eb395ddaa8ba762e6c4155ee3
yhjhhhgd <2384947346@qq.com>
2026-07-05 09:23:25 +0800

    Update economy.py

--- core/economy.py
+++ core/economy.py
@@ -1,22 +1,11 @@
-from database import get_session
-from models import User
 import random
 
 
 def reward(user):
     user.xp += random.randint(8, 18)
-    coins_before = user.coins
     user.coins += random.randint(2, 10)
     user.qi += random.randint(8, 15)
 
-    # TODO: 师徒分成（暂时关闭，避免报错）
-    # coins_gained = user.coins - coins_before
-    # if coins_gained > 0:
-    #     try:
-    #         from handlers.invite import give_master_share
-    #         ...
-    #     except:
-    #         pass   # 防止崩溃
 
 def level_up(user):
     while user.xp >= user.level * 500:
