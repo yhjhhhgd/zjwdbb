80d89cd0130f9afa400c73290a51c1e4be517f0d
yhjhhhgd <2384947346@qq.com>
2026-07-07 08:37:18 +0800

    Update models.py

--- models.py
+++ models.py
@@ -22,6 +22,8 @@ class User(Base):
     last_msg = Column(BigInteger, default=0)
     last_drop = Column(BigInteger, default=0)
     last_market_action = Column(BigInteger, default=0)
+    last_buy_action = Column(BigInteger, default=0)
+    last_sell_action = Column(BigInteger, default=0)
 
     cards = Column(
         MutableDict.as_mutable(JSON),
